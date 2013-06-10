356cee9aa924316eb3be0ee260d26730e0617f11
Tina Holly <tinacious.design@gmail.com>
2013-06-10 15:10:50 -0400

    grow, shrink and demo styles

---
+++ textShrinkGrow.js
@@ -0,0 +1,29 @@
+/*
+ *  Tinacious Design Text Shrink & Grow jQuery plugin
+ *  Plugin URL: https://github.com/tinacious/textSizer
+ *
+ *  Christina Holly (Tinacious Design)
+ *  http://tinaciousdesign.com
+ *
+ */
+(function($){
+	$.fn.textGrow = function() {
+		this.bind('click.textGrow', function(){
+			var currentFontSize = $('body').css('font-size');
+			currentFontSize = parseInt(currentFontSize);
+			var desiredFontSize = currentFontSize + 2;
+			$('body').css('font-size',desiredFontSize + 'px');
+		});
+	}
+})(jQuery);
+
+(function($){
+	$.fn.textShrink = function() {
+		this.bind('click.textShrink', function(){
+			var currentFontSize = $('body').css('font-size');
+			currentFontSize = parseInt(currentFontSize);
+			var desiredFontSize = currentFontSize - 2;
+			$('body').css('font-size',desiredFontSize + 'px');
+		});
+	}
+})(jQuery);
